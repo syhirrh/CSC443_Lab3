e2f3a3a663d4ab80da7d0968f9807f3aff188e1b
syhirrh <syasyahirah31@gmail.com>
2022-04-21 10:35:14 +0800

    second commit

--- comment.php
+++ comment.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<html>
 <body>
     <?php
-    // This is a comment
+    $x = 1// This is a comment
     // echo "X equals $x";
     $x += 10; // Increment $x by 10
     echo $x;
