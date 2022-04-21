6362223815fb4f128b52ab1b23cf5efd765d5c08
syhirrh <syasyahirah31@gmail.com>
2022-04-21 10:47:28 +0800

    first commit

---
+++ comment.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $x = 1;
+    // This is a comment
+    // echo "X equals $x";
+    $x += 10; // Increment $x by 10
+    echo $x;
+    /* This is a section
+    of multimedia comments
+    that will not be
+    interpreted */
+    ?>
+</body>
+</html>
