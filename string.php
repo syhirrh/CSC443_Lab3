ba1cf1c31a4597996331973e6f80512494a40f50
syhirrh <syasyahirah31@gmail.com>
2022-04-21 11:17:19 +0800

    second commit

--- string.php
+++ string.php
@@ -10,7 +10,7 @@
     <?php
     $txt1 = "Hello there, World!";
     $txt2 = "Have a nice day!";
-    $greet = $txt1 . " " . $txt2
+    $greet = $txt1 . " " . $txt2;
     echo $greet;?>
     echo "<br />";
     echo strlen("Hello world!");
