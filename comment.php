6684a44e12fe4f1985f5b5c44d5252cefe199a05
syhirrh <syasyahirah31@gmail.com>
2022-04-21 10:03:52 +0800

    second commit

--- comment.php
+++ comment.php
@@ -12,7 +12,9 @@
     // echo "X equals $x";
     $x += 10; // Increment $x by 10
     echo $x;
-    /* This is a section of multiline comments that will not be interpreted */
+    /* This is a section of multiline 
+    comments that will not be 
+    interpreted */
     ?>
 </body>
 </html>
